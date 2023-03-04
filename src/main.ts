ad38ae69ee2eb0e06a2bd1b02aa235638c31d0a0
Shubhanshoo Agarwal <sagarwal@instarem.com>
2023-03-04 06:01:18 +0530

    try catch block added

--- src/main.ts
+++ src/main.ts
@@ -5,18 +5,22 @@ import createMQProducer from "./queue/producer";
 const cityName = process.env.CITY;
 const amqpUrl = `${process.env.QUEUE_URL}#/`;
 const queueName = 'cities'
-console.log(process.env)
-const producer = createMQProducer(amqpUrl, queueName);
-const consumer = createMQConsumer(amqpUrl, queueName);
-consumer();
+try {
+    const producer = createMQProducer(amqpUrl, queueName);
+    const consumer = createMQConsumer(amqpUrl, queueName);
+    consumer();
 
-StreetsService.getStreetsInCity(cityName).then(res => {
-    const { streets } = res;
-    for (const street of streets) {
-        StreetsService.getStreetInfoById(street.streetId).then(res => {
-            producer(JSON.stringify(res));
-        })
-    }
-}).catch(err => {
-    console.error('Error while getting all streets in city', err);
-})
+    StreetsService.getStreetsInCity(cityName).then(res => {
+        const { streets } = res;
+        for (const street of streets) {
+            StreetsService.getStreetInfoById(street.streetId).then(res => {
+                producer(JSON.stringify(res));
+            })
+        }
+    }).catch(err => {
+        console.error('Error while getting all streets in city', err);
+    })
+} catch (err) {
+    console.log('Error while initiating the application', err);
+    process.exit(1);
+}
